16b10d7d0c99b1e81d9b7e06c6f18e9943abb3fe
sunildussoye <dussoyesunil@gmail.com>
2021-08-22 10:31:40 +0100

    Project creation

---
+++ src/main/java/com/sunil/munrotop/MunrotopApplication.java
@@ -0,0 +1,13 @@
+package com.sunil.munrotop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunrotopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunrotopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sunil/munrotop/MunrotopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunil.munrotop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunrotopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
